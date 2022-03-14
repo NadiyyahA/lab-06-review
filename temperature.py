e8113640402ea20d28651c86fa011c7ed34d16f7
Nadiyyah Ahmed <nadiyyah.ahmed@mail.citytech.cuny.edu>
2022-03-14 18:06:19 -0400

    completed temperature part of lab 06

--- temperature.py
+++ temperature.py
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <script>
-      var tempincelsius = prompt("Please enter a temperature in Celsius.");
-      var tempinfahrenhit = (tempincelsius * 1.8) + 32;
-      tempinfahrenheit = Math.round(tempinfahrenheit);
+tempincelsius = float(input("Please enter a temperature in Celsius. "))
+tempinfahrenhit = (tempincelsius * 1.8) + 32
+tempinfahrenheit = round(tempinfahrenheit)
 
-      console.log("The temperature is " + tempinfahrenheit + "F");
-    </script>
-  </body>
-</html>
+print("The temperature is " + str(tempinfahrenheit) + "°F")
